7bada902ff2ea58d39e0750489ea54deef08f183
Vincent Deo <vdeo@naoj.org>
2023-04-20 14:29:13 -1000

    Compile args for client-ony without ImageStreamIO

--- g2if_server/g2if/install.sh
+++ g2if_server/g2if/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-make
+echo "Build client..."
+make g2if_client
+echo "Try build server..."
+make # This will fail if ImageStreamIO is missing
+echo ""
+echo "Continuing..."
+make install-client
 make install
 
 make clean
