6c666f5649540abe0cbf5a65e1c30546e8c8407d
vdeo@AORTS <vdeo@naoj.org>
2024-05-24 06:31:32 +0000

    rts_starts fixes in the era of DM3k

--- scripts/rts_start.sh
+++ scripts/rts_start.sh
@@ -55,23 +55,13 @@ echo ""
 #------------------------------------------
 
 echo -e "${GREEN}Starting IIWI acquisition... ${ECOL}\n"
-cam-iiwistart
+camstart iiwi
 echo -e "${RED}Startup executed. Check apapane/iiwi SHM.${ECOL}\n"
 
 echo -e "${GREEN}Starting APD acquisition... ${ECOL}\n"
 python -m camstack.cams.ao_apd tmux -u 0
 
-echo -e "${RED}Startup executed. Check apapane/iiwi SHM.${ECOL}\n"
-
-echo -e "${GREEN}Starting APD acquisition... ${ECOL}\n"
-python -m camstack.cams.ao_apd tmux -u 0
-
-echo -e "${RED}Startup executed. Check apapane/iiwi SHM.${ECOL}\n"
-
-echo -e "${GREEN}Starting APD acquisition... ${ECOL}\n"
-python -m camstack.cams.ao_apd tmux -u 0
-
-echo -e "${RED}Startup executed. Check apapane/iiwi SHM.${ECOL}\n"
+echo -e "${RED}Startup executed. Check apd/curvature SHM.${ECOL}\n"
 
 # -----------------------------------------
 # Spin up the CACAO loop(s)
@@ -79,30 +69,33 @@ echo -e "${RED}Startup executed. Check apapane/iiwi SHM.${ECOL}\n"
 
 echo -e "${GREEN}Reconfiguring CACAO loops... ${ECOL}\n"
 
-echo -e "${RED}Done.${ECOL}\n"
-
 (cd ${HOME}/AOloop;
-rm -rf .apd188.cacaotaskmanager-log/;   # Loop 1
-rm -rf .lowfs188.cacaotaskmanager-log/; # Loop 2
-rm -rf .nir188.cacaotaskmanager-log/;   # Loop 3
-rm -rf .ttoff188.cacaotaskmanager-log/; # Loop 4
-cacao-loop-deploy ao3k-nirpyr188; # Loop 3 but also spins up DM00 / DM10
-cacao-loop-deploy ao3k-ttoff188;  # Loop 4 and spins up DM01 / DM11
-cacao-loop-deploy ao3k-apd188;    # Loop 1
-cacao-loop-deploy ao3k-lowfs188;  # Loop 2
+rm -rf .apd3k.cacaotaskmanager-log/;   # Loop 5
+rm -rf .lowfs3k.cacaotaskmanager-log/; # Loop 6
+rm -rf .nir3k.cacaotaskmanager-log/;   # Loop 7
+rm -rf .ttoff3k.cacaotaskmanager-log/; # Loop 8
+rm -rf .bimdm3kpt.cacaotaskmanager-log/; # Loop 9
+
+cacao-loop-deploy ao3k-nirpyr3k; # Loop 7 but also spins up D64 / DM65
+cacao-loop-deploy ao3k-ttoff3k;  # Loop 8 and spins up DM01 / DM11
+cacao-loop-deploy ao3k-apd3k;    # Loop 5
+cacao-loop-deploy ao3k-lowfs3k;  # Loop 6
+cacao-loop-deploy ao3k-bimdm3kpt; # Loop 9
 )
+
+echo -e "${RED}Done.${ECOL}\n"
 
 # -----------------------------------------
 # Starting up the DMCombs
 #------------------------------------------
 
-# TODO Not sure of that???
-(cd ${HOME}/AOloop/nir188-rootdir;
-cacao-aorun-001-dmsim start;
+# Start DM combs
+(cd ${HOME}/AOloop/nir3k-rootdir;
+#cacao-aorun-001-dmsim start;
 cacao-aorun-000-dm start
 )
-(cd ${HOME}/AOloop/ttoff188-rootdir;
-cacao-aorun-001-dmsim start;
+(cd ${HOME}/AOloop/ttoff3k-rootdir;
+#cacao-aorun-001-dmsim start;
 cacao-aorun-000-dm start
 )
 sleep 3.0
@@ -111,7 +104,21 @@ sleep 3.0
 # Start the DM communication - STEP 1: DM main (NGS)
 # --------------------------------------------------
 
-echo -e "${GREEN}Start DM communication... ${ECOL}\n"
+echo -e "${GREEN}Prepare ALPAO188 communication... ${ECOL}\n"
+# Assert DMch2disp-64 is running!! And that it has created dm64disp.
+ln -sf /milk/shm/dm64disp.im.shm /milk/shm/dm64in.im.shm
+
+rts-modeselect startmodule dm3k
+
+echo -e "${RED}Use rts-modeselect startmodule to start ALPAO control"
+
+echo -e "${GREEN}Prepare DM188 [passthrough] communication... ${ECOL}\n"
+#ln -sf /milk/shm/dac40_raw.im.shm /milk/shm/aol9_wfsim.im.shm
+
+ln -sf /milk/shm/dm64disp.im.shm /milk/shm/dm64in.im.shm
+
+echo -e "${GREEN}Prepare DM188 communication... ${ECOL}\n"
+
 
 # Assert DMch2disp-00 is running!! And that it has created dm00disp.
 ln -sf /milk/shm/dm00disp.im.shm /milk/shm/bim188_float.im.shm
@@ -130,10 +137,12 @@ creashmim wtt_telemetry 1 2 -z --type=f32
 
 creashmim dmvolt 1 188 -z --type=u16 # This one is to make dm_displouf happy.
 
-bim188-hwint lo # Start the loopback DAC40 interface
+#bim188-hwint lo # Start the loopback DAC40 interface
 
-echo -e "${BLUE}DM communication available TO LOOPBACK FAKE DAC40"
+rts-modeselect startmodule dac40
+
 echo -e "${BLUE}DM communication available on SHMs "bim188_float" "tt_value_float" "wtt_value_float" "ctt_value_float" ... ${ECOL}\n"
+echo -e "${RED}Use rts-modeselect startmodule to start DAC40 control"
 
 sleep 3
 
@@ -199,8 +208,10 @@ creashmim LO_tt 1 2 -z --type=f32
 creashmim LO_defocus 1 1 -z --type=f32
 
 # TODO
-${HOME}/src/rts/g2if_server/g2if/scripts/g2if_restart
+#${HOME}/src/rts/g2if_server/g2if/scripts/g2if_restart
+tmux new -d -s g2if
 sleep 3.0
+tmux send-keys -t g2if "python -m aorts.server.server_main" Enter
 
 echo -e "${BLUE}DM communication started ... ${ECOL}\n"
 echo -e "\n\n"
