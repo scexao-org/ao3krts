73d3bf01525dcc821294a1f2092875e623292eb8
vdeo@AORTS <vdeo@naoj.org>
2024-10-29 04:41:19 +0000

    Bugfix: painful segfault related to environment parsing + FPS lib being out of date and etc etc

--- aorts/cacao_stuff/cacaovars_reader.py
+++ aorts/cacao_stuff/cacaovars_reader.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-import glob
-import os
+import os, errno
 import subprocess
 import pathlib
 import re
@@ -9,6 +8,16 @@ import re
 Read environment from a subshell
 Stolen from https://stackoverflow.com/questions/1214496/how-to-get-environment-from-a-subprocess
 '''
+
+import shlex
+
+
+def exists(process):
+    try:
+        os.kill(process.pid, 0)
+    except OSError as e:
+        return False
+    return True
 
 
 def load_cacao_environment(cacaovars_path: pathlib.Path) -> dict[str, str]:
@@ -20,11 +29,20 @@ def load_cacao_environment(cacaovars_path: pathlib.Path) -> dict[str, str]:
 
     assert os.path.isfile(cacaovars_path) and cacaovars_path.is_absolute()
 
-    subproc = subprocess.Popen(
-            f'bash -c "source {cacaovars_path} > /dev/null; env"',
-            stdout=subprocess.PIPE, shell=True)
+    command = shlex.split(f"bash -c 'source {cacaovars_path} && env'")
+    #command = shlex.split(f"bash -c 'ls'")
+    subproc = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+    # IT SHOULD SUFFICE TO CALL .poll()
+    # https://python-list.python.narkive.com/Q2zUmGKI/trapping-the-segfault-of-a-subprocess-popen
+    # Because POpen was segfaulting upon communicate/exit, which is possible if POpen line has silently
+    # spawned children of its own
+    subproc.wait()
+
+    # Retrieve stdout from bash source
     lines = subproc.communicate()[0].decode().split('\n')
 
+    # Parse environment
     regex_cacao_env = re.compile('^(CACAO_.*)=(.*)$')
 
     output_env: dict[str, str] = {}
