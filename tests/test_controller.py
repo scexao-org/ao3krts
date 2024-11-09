809ce2a8e662fabbdbd2afaa179f125be2b44940
vdeo@AORTS <vdeo@naoj.org>
2024-11-09 04:04:23 +0000

    [tests] add stubs for (upcoming) LGS focus offloader

---
+++ tests/test_controller.py
@@ -0,0 +1,53 @@
+import pytest
+
+from aorts.control.foc_offloader import FocusLGSOffloader
+
+
+class FocusLGSOffloaderSpoof(FocusLGSOffloader):
+    FPS_NAME = 'foc_offl_test'
+
+
+@pytest.fixture(scope="module")
+def focusLGSOffloader(request):
+
+    foc_ctrl = FocusLGSOffloaderSpoof()
+
+    def teardown():
+        foc_ctrl.fps.disconnect()
+        foc_ctrl.fps.destroy()
+
+    request.addfinalizer(teardown)
+
+    return foc_ctrl
+
+
+def test_focoff_basic(focusLGSOffloader):
+    assert focusLGSOffloader.fps is not None
+
+    assert "in_stream" in focusLGSOffloader.fps.key_types
+
+    focusLGSOffloader.set_input_stream('yolo')
+    assert focusLGSOffloader.fps['in_stream'] == 'yolo'
+
+
+def test_test_passes():
+    assert True
+
+
+def test_test_fails():
+    assert False
+
+
+def test_test_raises_w_success():
+    with pytest.raises(Exception) as e_info:
+        s = 1 / 0
+
+
+def test_test_raises_something_else():
+    with pytest.raises(OSError) as e_info:
+        x = 1 / 0
+
+
+def test_test_fails_to_raise():
+    with pytest.raises(OSError) as e_info:
+        x = 1 / 1
