dfc6f0f03f3fb20c87a6d4a48cda9b71e0e6fffe
vdeo <vdeo@naoj.org>
2024-11-26 18:27:25 -1000

    [cacao][tests] Add confstart/confstop status waits, finalize cleanup of cacao loop deploy test.

--- tests/cacao_stuff/cacaovars_reader_test.py
+++ tests/cacao_stuff/cacaovars_reader_test.py
@@ -9,26 +9,23 @@ def test_cacaoconfig_fixt(ctfixt_parse_config):
     files = glob.glob('/milk/shm/*')
     assert len(files) == 0
 
-    return
-
     loop_mgr = CacaoLoopManager('ao3k-nirpyr3k-testonly', None,
                                 root_all=ctfixt_parse_config)
 
     loop_mgr.fps_ctrl.rescan_all()
+    loop_mgr.confstop_processes(5.0)
     assert all([
             not f.conf_isrunning()
             for f in loop_mgr.fps_ctrl.fps_cache.values()
     ])
-    loop_mgr.confstart_processes()
+    loop_mgr.confstart_processes(5.0)
     assert all([
             f.conf_isrunning() for f in loop_mgr.fps_ctrl.fps_cache.values()
     ])
-    loop_mgr.confstop_processes()
+    loop_mgr.confstop_processes(5.0)
     assert all([
             not f.conf_isrunning()
             for f in loop_mgr.fps_ctrl.fps_cache.values()
     ])
 
-    loop_mgr.rootdir
-
-    assert False
+    assert str(loop_mgr.rootdir).startswith('/tmp/pytest')
