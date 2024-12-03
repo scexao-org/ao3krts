226ee0ae65a79deec02a1979a3217a75c37ca379
vdeo@AORTS <vdeo@naoj.org>
2024-12-03 01:48:26 +0000

    Fix cacao loop deploy test and missing simLHS folder in test config.

--- tests/cacao_stuff/cacaovars_reader_test.py
+++ tests/cacao_stuff/cacaovars_reader_test.py
@@ -6,11 +6,19 @@ from aorts.cacao_stuff.loop_manager import CacaoLoopManager
 def test_cacaoconfig_fixt(ctfixt_parse_config):
 
     import glob
-    files = glob.glob('/milk/shm/*')
+    files = glob.glob('/milk/shm/aol107*.im.shm')
     assert len(files) == 0
+
+    import os
+    files = glob.glob(os.environ['MILK_SHM_DIR'] + '/aol107*.im.shm')
+    assert len(files) > 0
 
     loop_mgr = CacaoLoopManager('ao3k-nirpyr3k-testonly', None,
                                 root_all=ctfixt_parse_config)
+
+    assert os.path.isdir(loop_mgr.conf_folder)
+    assert os.path.isdir(loop_mgr.rootdir)
+    assert os.path.isdir(loop_mgr.rundir)
 
     loop_mgr.fps_ctrl.rescan_all()
     loop_mgr.confstop_processes(5.0)
