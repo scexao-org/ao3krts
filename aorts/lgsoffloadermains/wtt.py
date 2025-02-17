356c6555df047c360afed2901e9df4b84f9e3eb4
vdeo <vdeo@naoj.org>
2025-02-17 09:17:00 +0100

    [wtt] Fix offsets and naming and LGS orientation [ono]

--- aorts/lgsoffloadermains/wtt.py
+++ aorts/lgsoffloadermains/wtt.py
@@ -18,13 +18,15 @@ from pyMilk.interfacing.shm import SHM
 from ..control.wtt_offloader import WTTOffloaderFPS
 
 WTT_TO_NGSMODE_MATRIX = np.array([[-0.3, -0.3], [-0.3, +0.3]], dtype=np.float32)
-
 NGSMODEVAL_TO_WTT_MATRIX = np.linalg.inv(WTT_TO_NGSMODE_MATRIX)
+
+WTT_TO_LGSMODE_MATRIX = np.array([[+0.3, +0.3], [+0.3, -0.3]], dtype=np.float32)
+LGSMODEVAL_TO_WTT_MATRIX = np.linalg.inv(WTT_TO_LGSMODE_MATRIX)
 
 
 @click.command('main_wtt_offloader')
 @click.argument('fps_name', type=str, default='wtt_offl')
-def main_foc_offloader(fps_name: str = 'wtt_offl'):
+def main_wtt_offloader(fps_name: str = 'wtt_offl'):
 
     # For logging
     init_logger_autoname()
@@ -57,8 +59,8 @@ def main_foc_offloader(fps_name: str = 'wtt_offl'):
             last_print_time = time.time()
 
         if fps.loopZERO:
-            fps.out_tip = 0.0
-            fps.out_til = 0.0
+            fps.out_tip = 5.0
+            fps.out_til = 5.0
             fps.loopZERO = False
 
         if not fps.loopON:
@@ -70,7 +72,7 @@ def main_foc_offloader(fps_name: str = 'wtt_offl'):
         if buffer is None:
             continue
 
-        tip, til = NGSMODEVAL_TO_WTT_MATRIX @ buffer[:2]
+        tip, til = LGSMODEVAL_TO_WTT_MATRIX @ buffer[:2]
 
         frate_gain = 0.001
 
@@ -79,15 +81,17 @@ def main_foc_offloader(fps_name: str = 'wtt_offl'):
         last_data_time = _now
 
         # Integrator
-        output = shm_output_antiwindup.get_data()
+        # output = shm_output_antiwindup.get_data() # change to shm_output by YO 2025/02/16
+        output = shm_output.get_data()
+
         out_tip, out_til = output[0], output[1]
 
         out_tip = out_tip * fps.loopmult + tip * fps.loopgain
         out_til = out_til * fps.loopmult + til * fps.loopgain
 
         # Remember to offset by 5 because this is WTT!!!
-        out_tip = np.clip(out_tip, -fps.looplimit, fps.looplimit) + 5
-        out_til = np.clip(out_til, -fps.looplimit, fps.looplimit) + 5
+        out_tip = np.clip(out_tip, -fps.looplimit, fps.looplimit)
+        out_til = np.clip(out_til, -fps.looplimit, fps.looplimit)
 
         fps.out_tip, fps.out_til = out_tip, out_til
         fps.m_frate = fps.m_frate * (1 - frate_gain) + frate_gain / interval
@@ -98,4 +102,4 @@ def main_foc_offloader(fps_name: str = 'wtt_offl'):
 
 
 if __name__ == '__main__':
-    main_foc_offloader()
+    main_wtt_offloader()
